47f7213695118c201aa86f0241a21623bc91b705
MacBook Pro <naym100m@gmail.com>
2024-12-16 18:32:03 +0600

    [almost initial setup] compleate

--- curate-north-backend/src/app.ts
+++ curate-north-backend/src/app.ts
@@ -1,11 +1,15 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import router from './app/routes';
 
 const app: Application = express();
 
 // Parsers
 app.use(express.json());
 app.use(cors());
+
+// Application routes
+app.use('/api', router);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello 🤙 Curate North ...');
